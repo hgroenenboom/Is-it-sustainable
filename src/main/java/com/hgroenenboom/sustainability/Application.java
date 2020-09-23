950420cbbe97120d2a24b6da09b3528e827c1331
Harold Groenenboom <HAROLD_GROE@hotmail.com>
2020-09-23 17:59:34 +0200

    First commit: Empty Spring project

---
+++ src/main/java/com/hgroenenboom/sustainability/Application.java
@@ -0,0 +1,16 @@
+package com.immdb.moviecatalogue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootApplication
+@PropertySource(value = "file:./application.properties")
+public class Application {
+
+    public static String role = "none";
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/com/hgroenenboom/sustainability/data/Rating.java
@@ -0,0 +1,4 @@
+package com.immdb.moviecatalogue.data;
+
+public class Rating {
+}
